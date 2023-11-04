b6836fbf8dc4a3b15538e3cebb5253854f633378
mith@mmk <47620686+mith-mmk@users.noreply.github.com>
2023-11-04 11:21:33 +0900

    error trap

--- modules/img2txt2img.py
+++ modules/img2txt2img.py
@@ -28,33 +28,45 @@ def img2txt2img(
     params = []
     for imgfile in imagefiles:
         Logger.info(f"Processing {imgfile}")
-        param = create_img2txt(imgfile)
+        try:
+            param = create_img2txt(imgfile)
 
-        # If enable_hr is True, set width and height to firstphase_width and firstphase_height
-        if param.get("enable_hr", False):
-            if "firstphase_width" in param:
-                param["width"] = param["firstphase_width"]
-                del param["firstphase_width"]
-            if "firstphase_height" in param:
-                param["height"] = param["firstphase_height"]
-                del param["firstphase_height"]
+            # If enable_hr is True, set width and height to firstphase_width and firstphase_height
+            if param.get("enable_hr", False):
+                if "firstphase_width" in param:
+                    param["width"] = param["firstphase_width"]
+                    del param["firstphase_width"]
+                if "firstphase_height" in param:
+                    param["height"] = param["firstphase_height"]
+                    del param["firstphase_height"]
 
-        for key in overrides:
-            param[key] = overrides[key]
+            for key in overrides:
+                param[key] = overrides[key]
 
-        param["seed"] = int(param["seed"]) + seed_diff
-        overrideSettings = param.get("override_settings")
+            if "seed" in param:
+                if int(param["seed"]) >= 0:
+                    param["seed"] = int(param["seed"]) + seed_diff
+                else:
+                    Logger.warning("Seed is illegal")
+                    param["seed"] = -1
+            else:
+                Logger.warning("No seed in param")
+                param["seed"] = -1
+            overrideSettings = param.get("override_settings")
 
-        # If infomation VAE is not filename, get from model name to vae filename dict
-        if overrideSettings is None:
-            if "sd_vae" in overrideSettings:
-                modelHash = overrideSettings.get("sd_model_checkpoint")
-                if modelHash in modeldict:
-                    modelName = modeldict[modelHash]["model_name"]
-                    vae = models.get(modelName, [None])[0]
-                    if vae is not None:
-                        param["override_settings"]["sd_vae"] = vae
-        params.append(param)
+            # If infomation VAE is not filename, get from model name to vae filename dict
+            if overrideSettings is None:
+                if "sd_vae" in overrideSettings:
+                    modelHash = overrideSettings.get("sd_model_checkpoint")
+                    if modelHash in modeldict:
+                        modelName = modeldict[modelHash]["model_name"]
+                        vae = models.get(modelName, [None])[0]
+                        if vae is not None:
+                            param["override_settings"]["sd_vae"] = vae
+            params.append(param)
+        except Exception as e:
+            Logger.error(f"Failed to create img2txt params {e}")
+            continue
     Logger.info("txt2img start")
     if not dry_run:
         txt2img(params, base_url=base_url, output_dir=output_dir, opt=opt)
