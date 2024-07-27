00943a2f50e4b4a646e3ed7a7c3cbab917912ed6
MITH@mmk <47620686+mith-mmk@users.noreply.github.com>
2024-07-27 15:25:28 +0900

    Refactor get_part function to handle part numbers with hyphens

--- modules/util.py
+++ modules/util.py
@@ -4,7 +4,16 @@ import os
 def get_part(filename):
     try:
         part = os.path.basename(filename).split("part")[-1].split(".")[0]
-        part = "part" + part
+        parts = part.split("-")
+        parts.reverse()
+        part = ""
+        for p in parts:
+            if not p.isdigit():
+                part = p + "-" + part
+            else:
+                break
+        part = part[:-1]
+
     except Exception as e:
         print(f"Failed to get part {e}")
         part = ""
